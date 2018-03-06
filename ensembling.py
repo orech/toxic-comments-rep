fa29b3d81f01104561956936b623b8aa59c950bb
Ilya Sklyar <sklyar@i6.informatik.rwth-aachen.de>
2018-03-06 12:45:05 +0100

    Changed ensembling

--- ensembling.py
+++ ensembling.py
@@ -1,40 +1,37 @@
 import pandas as pd
 import argparse
 import os.path
+from os import listdir
 import numpy as np
 
 target_labels = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
 num_classes = len(target_labels)
+num_test_samples = 153164
 
 def main():
-    print("Stsrted")
-    parser = argparse.ArgumentParser(description='--gru=$GRU --cnn=$CNN --attention=$ATTEND')
-    parser.add_argument('gru-submission')
-    parser.add_argument('cnn-submission')
-    parser.add_argument('attention-submission')
-    args = parser.parse_args()
 
+    submission_path = '../submissions'
     result_path = './outputs/'
     if not os.path.exists(result_path):
         os.mkdir(result_path)
 
-    gru_sub = pd.read_csv('best_subs/2BiGRU_GlobMaxPool_folds.submit')
-    cnn_sub = pd.read_csv('best_subs/original_pyramidCNN_folds.csv')
-    attention_sub = pd.read_csv('best_subs/BiGRU_attention_folds.submit')
-    gru_probs = np.array(gru_sub[target_labels].values)
-    cnn_probs = np.array(cnn_sub[target_labels].values)
-    attention_probs = np.array(attention_sub[target_labels].values)
+    final_predictions = np.ones([num_test_samples, num_classes])
+    i = 0
+    for file in listdir(submission_path):
+        file = os.path.join(submission_path,file)
+        df = pd.read_csv(file)
+        probs = np.array(df[target_labels].values)
+        final_predictions *= probs
+        i += 1
+    final_predictions = final_predictions ** (1/i)
 
-    final_predictions = (gru_probs * cnn_probs * attention_probs) ** (1/3)
-
-
-    test_ids = gru_sub["id"].values
+    test_ids = df["id"].values
     test_ids = test_ids.reshape((len(test_ids), 1))
 
     test_predicts = pd.DataFrame(data=final_predictions, columns=target_labels)
     test_predicts["id"] = test_ids
     test_predicts = test_predicts[["id"] + target_labels]
-    submit_path = os.path.join(result_path, "{0}.csv".format("gru_cnn_attend_ensemble.csv"))
+    submit_path = os.path.join(result_path, "{0}.csv".format("ensemble"))
     test_predicts.to_csv(submit_path, index=False)
 if __name__=='__main__':
     main()
